f07816b026e61e846c6c2d5d6cbd371630e8869d
WonderBlunders <72946028+wonderblunders@users.noreply.github.com>
2021-02-08 21:28:08 +0530

    08feb

--- ripl/settings.py
+++ ripl/settings.py
@@ -29,7 +29,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.126.239.17']
 EMAIL_HOST_USER='yogibabanet@gmail.com'
 EMAIL_HOST_PASSWORD= '@yogibabanet'
 # Application definition
